900a0d6a7a94ee803945a0db80fc72d59465d5b7
carldamey <carldamey@gmail.com>
2023-10-16 23:54:06 -0700

    adds /home route

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require("express")
-const app = express()
-
+// Require modules
+const express = require("express");
+  
+// Create the Express app
+const app = express();
+  
+// Configure the app (app.set)
+  
+  
+// Mount middleware (app.use)
+  
+  
+// Mount routes
 app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-})
+  res.send("<h1>Hello Express!</h1>");
+});
 
-app.listen(3000, function () {
-    console.log("Listening on port 3000")
+app.get("/home", function (req, res) {
+    res.send("<h1>Home Page<h1>")
 })
+  
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log("Listening on port 300");
+});
