ef0a5e4c2ef64fbf1d275135303e7a5d5a2e1687
carldamey <carldamey@gmail.com>
2023-10-17 00:40:47 -0700

    creates mock database and parent directory for todo-db.js

---
+++ data/todo-db.js
@@ -0,0 +1,7 @@
+const todos = [
+    {skill: "JavaScript", learned: true},
+    {skill: "React.js", learned: false},
+    {skill: "Django", learned: false},
+    {skill: "Python", learned: true},
+    {skill: "Node.js", learned: null}
+  ]
